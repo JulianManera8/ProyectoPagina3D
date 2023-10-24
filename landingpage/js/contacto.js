942405f096313bca4ad8b899b9c6fd395c5120ec
JulianManera8 <144916371+JulianManera8@users.noreply.github.com>
2023-10-24 20:42:12 -0300

    Update contacto.js

--- landingpage/js/contacto.js
+++ landingpage/js/contacto.js
@@ -176,9 +176,9 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 //VARIABLES
-
 const carrito = document.querySelector('#carrito');
-const listaCursos = document.querySelector('#lista-productos');
+const listaProductos = document.querySelector('#lista-productos');
+const listaProductos2 = document.querySelector('.lista-productos');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const botonesCarrito = document.querySelector('.botones-carrito');
 const botonVaciarCarrito = document.querySelector('.vaciar-carrito');
@@ -188,12 +188,17 @@ const divSubtotal = document.querySelector('.subtotal');
 const spanSubtotal = document.querySelector('.precio-subtotal');
 const iconoCarrito = document.querySelector('#img-carrito');
 const imgCarrito = document.getElementById('img-carrito');
+const contadorCarrito = document.getElementById('contadorCarrito');
 const imgCerrarCarrito = document.getElementById('img-close');
 const submenuCarrito = document.querySelector('.submenu');
 const spanTotal = document.querySelector('#totalCarrito');
 const ofertasEspeciales = document.getElementById('listadoOfertas') 
 const btnOfertas = document.getElementById('btn-ofertas')
 const btnOfertas2 = document.getElementById('btn-ofertas2')
+const irAlBlog = document.querySelector('.a-pMostrarEnvio');
+const blogEnvio = document.querySelector('.infoEnvio');
+const irModelosFooter = document.querySelector('.irModelosFooter')
+const modelosFooter = document.querySelector('.modelosFooter');
 
 
 let totalCarrito = 0;
@@ -214,7 +219,7 @@ crearEventos();
 function crearEventos() {
     
     //agregar producto seleccionado al carrito
-    listaCursos.addEventListener('click', agregarCurso);
+    listaProductos.addEventListener('click', agregarCurso);
 
     //borrar producto desde la X desde el carrito
     contenedorCarrito.addEventListener('click', borrarProducto);
@@ -223,9 +228,15 @@ function crearEventos() {
     botonVaciarCarrito.addEventListener('click', (e) => {
 
         e.preventDefault();
+
         arregloProductos = [];
+        if (arregloProductos.length === 0) {
+            contadorCarrito.innerHTML = '0';
+        }
+        
         crearHTML();
         sacarBotones();
+        
 
     });
 
@@ -234,31 +245,68 @@ function crearEventos() {
         arregloProductos = JSON.parse(localStorage.getItem('producto')) || [];
 
         crearHTML();
+        contador(arregloProductos);
     });
 
 
     //que no me muestre los botones de vaciar o pagar si no tengo nada en el carrito
     iconoCarrito.addEventListener('mouseenter', sacarBotones);
 
+    //que se ilumine el blog donde dice la info del carrito
+    irAlBlog.addEventListener('click', iluminarIcon);
+
+
 
 }
+
+
+
+
+
+//iluminar el blog del envio
+function iluminarIcon() {
+    
+    blogEnvio.classList.add('animacionIcon')    
+    
+    setTimeout(() => {
+        blogEnvio.classList.remove('animacionIcon')    
+    }, 5000);
+       
+}
+
+function iluminarFooter() {
+    
+    modelosFooter.classList.add('iluminarFooter')   
+    setTimeout(() => {
+        modelosFooter.classList.add('desiluminarFooter')    
+    }, 3000);
+}
+
 
 //mostrar y cerrar carrito
 imgCarrito.onclick = () => {
     submenuCarrito.classList.add('active');
     submenuCarrito.style.transition = ('all 0.5s');
     submenuCarrito.style.transform = ('translateX(-500px)');
+
+    contadorCarrito.classList.add('esconder');
+
     imgCarrito.classList.add('esconder');   
+
     imgCerrarCarrito.classList.remove('esconder');
-    iconoCarrito.classList.remove('agregado');
+
 }
 
 imgCerrarCarrito.onclick = () => {
     submenuCarrito.style.transition = ('all 0.75s');
     submenuCarrito.style.transform = ('translateX(500px)');
     submenuCarrito.classList.add('cerrar');
+
     imgCerrarCarrito.classList.add('esconder')
+
     imgCarrito.classList.remove('esconder') 
+
+    contadorCarrito.classList.remove('esconder');
 }
 
 //que no hata botones de pagar y vaciar si no hay elementos
@@ -275,7 +323,7 @@ function sacarBotones() {
         const nadaCarrito = document.createElement('p');
         nadaCarrito.innerHTML = 'No hay nada en el carrito.';
         nadaCarrito.innerHTML = 
-        '<p> No hay nada en el carrito</p><a href="productos.html" class="iracomprar" > Ir a PRODUCTOS </a>';
+        '<p> No hay nada en el carrito</p><a href="productos.html#verProductos" class="iracomprar" > Ir a PRODUCTOS </a>';
 
         botonesCarrito.style.alignItems = 'center';
         divNadaCarrito.appendChild(nadaCarrito); 
@@ -298,11 +346,24 @@ function agregarCurso(e) {
         const productoSeleccionado = e.target.parentElement.parentElement;
         leerDatoProducto(productoSeleccionado);
         iconoCarrito.classList.add('agregado');
+        contadorCarrito.classList.add('agregado');
+        contadorCarrito.classList.add('agregado-numero');
         e.target.textContent = 'AGREGADO';
+
+        setTimeout(() => {
+            e.target.textContent = 'Agregar al carrito';
+        }, 2500);
         
         setTimeout(() => {
             e.target.textContent = 'Agregar al carrito';
-        }, 2250);
+            iconoCarrito.style.transition = '0.5s all';
+            contadorCarrito.style.transition = '0.5s all';
+
+            iconoCarrito.classList.remove('agregado');
+            contadorCarrito.classList.remove('agregado');
+            contadorCarrito.classList.remove('agregado-numero');
+        }, 4000);
+        
     }
     
 };
@@ -340,6 +401,8 @@ function leerDatoProducto(producto) {
     } else {
         arregloProductos = [...arregloProductos, infoProducto];   
     }
+
+    
     
     //le pasamos el arreglo a la funcion crearHTML para justamente crear el html del carrito
     crearHTML(producto.precio, infoProducto);
@@ -347,6 +410,10 @@ function leerDatoProducto(producto) {
 
 
 function crearHTML(precio) {
+    //contador
+    contador(arregloProductos)
+    console.log(arregloProductos)
+
     //primero hay que hacer que no se dupliquen
     
     limpiarHTML();
@@ -395,6 +462,7 @@ function crearHTML(precio) {
 
         //que me agregue los precios cada vez q sumo algo al subtotal
         subtotalCarrito = subtotalCarrito + parseInt(producto.precio)
+        
 
     });
 
@@ -417,6 +485,21 @@ function sincronizarStorage() {
     localStorage.setItem('producto', JSON.stringify(arregloProductos))
 }
 
+function contador(arregloProductos) {
+    let numeroContador = arregloProductos.length
+    
+    arregloProductos.forEach( producto => {
+        //numeroContador.innerHTML = numeroContador++;
+        contadorCarrito.innerHTML = numeroContador;
+
+    
+    });
+    
+
+    
+    
+}
+
 
 //borrar producto del carrito desde la X
 function borrarProducto(e) {
@@ -431,7 +514,14 @@ function borrarProducto(e) {
     //luego le pasamos d nuevo la funcion que crea el html para que borre el q tocamos    
     crearHTML();
     sacarBotones();
+    if (arregloProductos.length === 0) {
+        contadorCarrito.innerHTML = '0';
+    }
 };
+
+
+
+
 
 function agregarBotones() {
     botonPagarCarrito.classList.remove('esconder')
@@ -466,6 +556,7 @@ const btnCalcularEnvio1 = document.getElementById('calcularEnvio1');
 const btnCalcularEnvio2 = document.getElementById('calcularEnvio2')
 const spanEnvio = document.querySelector('.numero-envioTotal')
 const opciones = document.querySelectorAll('input[name="opciones"]');
+const formularioEnvio = document.querySelector('.formularioEnvio');
 let opcionSeleccionada;
 let totalEnvio = 0;
 
@@ -480,6 +571,7 @@ function eventosEnvio() {
     //volver a setear el calculo de envio
     btnProbarOtro.addEventListener('click', resetCalculo)
 
+    
 
 }
 
@@ -489,9 +581,11 @@ function mostrarCalcularEnvio() {
     spanEnvio.textContent = '...';
 }
 
-function leerCheck() {
+function leerCheck(e) {
+    e.preventDefault();
 
-    for (const opcion of opciones) {
+    for (let opcion of opciones) {
+
         if (opcion.checked) {
             opcionSeleccionada = opcion.value;
 
@@ -542,34 +636,26 @@ function leerCheck() {
                 spanTotal.classList.remove('esconder')
             }
 
-            break; // Termina la iteración cuando encuentres la opción seleccionada
+            
         }
+
     }
-    //spanTotal.textContent = spanSubtotal+spanEnvio
+
     
 }
 
 function resetCalculo() {
+
     mostrarCalcularEnvio();
-    opciones.forEach(opcion => {
-        opcion.checked = false
-    });
+
     btnProbarOtro.classList.add('esconder')
     spanTotal.classList.add('esconder')
-    
-    alert('Si quieres completar el calculo del envio de nuevo, refresca la pagina. Y no te preocupes, se te guardarán los productos del carrito. Sinó, solo dale a ACEPTAR y modifica lo ultimo que seleccionaste.')
-    
-
 
 }
 
 function calcularTotal() {
     spanTotal.textContent = parseInt(spanSubtotal.textContent) + totalEnvio
 }
-
-
-
-
 
 
 
